604d1da5cf07ed7918cbf571d8677764072bd455
Claus <renessmi2018@gmail.com>
2019-12-20 20:01:19 -0800

    trying images with loops

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -56,7 +56,7 @@ class BotHandler:
 # In[3]:
 
 
-token = os.environ.get('TELEGRAM_TOKEN') #Token of your bot
+token = '938870264:AAHAL4RlulLbjEcgM1awC9eDoyHUtLa4jsQ'#os.environ.get('TELEGRAM_TOKEN') #Token of your bot
 magnito_bot = BotHandler(token) #Your bot's name
 
 
@@ -92,26 +92,26 @@ def main():
                     magnito_bot.send_message(first_chat_id, f"Hey, {first_chat_name}, what's up?\n\nI am SEO Searcher, and I can search for different lessons from SOE by using keywords from the topics.\nI have all the lessons from the beginning to November 2019 in my database.\nHere are a few tips how to use me:\n1) Enter the keywords you can remember from a topic. As a separator use SPACE.\n(Warning, in one message you can write only keywords from THE SAME topic, otherwise I will send you nothing)\n2) The more keywords, the more specific the search.\n\nFor instance, you can write 'exposed to' or just 'exposed'.\nYou can mention a few key words from ONE topic, such as 'disappointed  devoted to discriminated' or 'much many'.\n\nNow send me something!")
                     new_offset = first_update_id + 1
                     
-                    print("Current directory: "+os.getcwd()+"\n")
-                    print("List of files in the current directory:\n")
-                    for file in os.listdir():
-                    	print(file)
-                    continue
+                    #print("Current directory: "+os.getcwd()+"\n")
+                    #print("List of files in the current directory:\n")
+                    #for file in os.listdir():
+                   # 	print(file)
+                   # continue
 
-#                 user_input = set((first_chat_text.lower()).split(' ')) # split and convert user input to set 
-#                 txt_list = os.listdir('txt') # list of txt files in derectory
-#                 found_flag = True
-#                 for txt_f in txt_list: # grabbing all txt files in the directory
-#                     f = open("txt/"+txt_f,"r")
-#                     text = set(f.read().split()) # getting a bag of words from the txt's
-#                     if user_input.issubset(text): # check if the user input is a subset of the bag of words
-#                         magnito_bot.send_photo(first_chat_id,txt_f[:-4]) # sending a corresponding picture
-#                         new_offset = first_update_id + 1
-#                         found_flag = False
-#                     f.close()
-#                 if found_flag:
-#                     magnito_bot.send_message(first_chat_id, 'Sorry, there is nothing like that. Try something else!')
-#                     new_offset = first_update_id + 1
+                user_input = set((first_chat_text.lower()).split(' ')) # split and convert user input to set 
+                txt_list = os.listdir('txt') # list of txt files in derectory
+                found_flag = True
+                for txt_f in txt_list: # grabbing all txt files in the directory
+                    f = open("txt/"+txt_f,"r")
+                    text = set(f.read().split()) # getting a bag of words from the txt's
+                    if user_input.issubset(text): # check if the user input is a subset of the bag of words
+                        magnito_bot.send_photo(first_chat_id,txt_f[:-4]) # sending a corresponding picture
+                        new_offset = first_update_id + 1
+                        found_flag = False
+                    f.close()
+                if found_flag:
+                    magnito_bot.send_message(first_chat_id, 'Sorry, there is nothing like that. Try something else!')
+                    new_offset = first_update_id + 1
 
 
 
